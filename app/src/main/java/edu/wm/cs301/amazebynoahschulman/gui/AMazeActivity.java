a75f567b551a8e65827c5c9613a0dd357af466ec
noahschulman14 <noahschulman14@wm.edu>
2022-11-21 13:06:41 -0500

    Generated base files.

---
+++ app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/AMazeActivity.java
@@ -0,0 +1,28 @@
+package edu.wm.cs301.amazebynoahschulman.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.wm.cs301.amazebynoahschulman.R;
+
+public class AMazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amaze);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), GeneratingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/GeneratingActivity.java
@@ -0,0 +1,16 @@
+package edu.wm.cs301.amazebynoahschulman.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.wm.cs301.amazebynoahschulman.R;
+
+public class GeneratingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generating);
+    }
+}
