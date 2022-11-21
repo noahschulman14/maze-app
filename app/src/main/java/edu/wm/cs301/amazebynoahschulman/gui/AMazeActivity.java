28e10c9cd7ba47ea62c2ecb18332e498275add44
noahschulman14 <noahschulman14@wm.edu>
2022-11-21 14:29:18 -0500

    Created "Revisit" and "Explore" buttons on AMazeActivity state screen linking to GeneratingActivity state screen.

--- app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/AMazeActivity.java
@@ -6,23 +6,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import edu.wm.cs301.amazebynoahschulman.R;
 
 public class AMazeActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView seekBarProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amaze);
 
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        // this is for the exploreButton, moves it to GeneratingActivity when clicked
+        final Button exploreButton = findViewById(R.id.exploreButton);
+        exploreButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
                 Intent intent = new Intent(getApplicationContext(), GeneratingActivity.class);
                 startActivity(intent);
             }
         });
+
+        // this is for the revisitButton, moves it to GeneratingActivity when clicked
+        final Button revisitButton = findViewById(R.id.revisitButton);
+        revisitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), GeneratingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // making the seek bar's value update on screen in a textview
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBarProgress = (TextView) findViewById(R.id.seekBarProgress);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                seekBarProgress.setText("SIZE: "+ progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 }
