8d7cb778b5da771238cbb26a4073ec4b16ff928a
noahschulman14 <noahschulman14@wm.edu>
2022-11-21 16:48:44 -0500

    added radio buttons to select driver for GeneratingActivity

--- app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/GeneratingActivity.java
@@ -3,14 +3,35 @@ package edu.wm.cs301.amazebynoahschulman.gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import edu.wm.cs301.amazebynoahschulman.R;
 
 public class GeneratingActivity extends AppCompatActivity {
 
+    // field variable to store which driver is being used
+    private int driver;
+    // field variable to store which robot config is being used
+    private int robotConfig;
+
+    private RadioGroup driverRadioGroup;
+    private RadioButton driverRadioButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generating);
+
+        // implementing driver radio group:
+        driverRadioGroup = findViewById(R.id.radioGroup);
+
+    }
+
+    public void checkButton(View v) {
+        driver = driverRadioGroup.getCheckedRadioButtonId();
+        driverRadioButton = findViewById(driver);
     }
 }
