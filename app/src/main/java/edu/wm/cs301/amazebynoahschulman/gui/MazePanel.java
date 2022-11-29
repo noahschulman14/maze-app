77130f0098f37b921f61dbf5e75e91f30a030aaf
noahschulman14 <noahschulman14@wm.edu>
2022-11-29 13:39:37 -0500

    Implemented addLine, addFilledOval, addArc, addMarker methods for MazePanel

--- app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/MazePanel.java
+++ app/src/main/java/edu/wm/cs301/amazebynoahschulman/gui/MazePanel.java
@@ -150,7 +150,7 @@ public class MazePanel extends View implements P7PanelF22{
         // first need to make the Paint fill
         paint.setStyle(Paint.Style.FILL);
         // now need to put a rectangle in specified part of canvas
-        canvas.drawRect((float)x, (float)y, (float)x + width, (float)y + height, paint);
+        canvas.drawRect((float)x, (float)y, (float)x + (float)width, (float)y + (float)height, paint);
         //                                                          ******SHOULD THIS BE Y - HEIGHT??? ******
     }
 
@@ -178,7 +178,7 @@ public class MazePanel extends View implements P7PanelF22{
         // using moveTo method for the first point
         polygonPath.moveTo(xPoints[0], yPoints[0]);
         // for the rest of the points, use lineTo method
-        for (int i = 1; i < xPoints.length; i++) {
+        for (int i = 1; i < nPoints; i++) {
             polygonPath.lineTo(xPoints[i], yPoints[i]);
         }
         // then use lineTo for the first point
@@ -187,34 +187,141 @@ public class MazePanel extends View implements P7PanelF22{
         canvas.drawPath(polygonPath, paint);
     }
 
+    /**
+     * Adds a polygon.
+     * The polygon is not filled.
+     * The polygon is specified with {@code (x,y)} coordinates
+     * for the n points it consists of. All x-coordinates
+     * are given in a single array, all y-coordinates are
+     * given in a separate array. Both arrays must have
+     * same length n. The order of points in the arrays
+     * matter as lines will be drawn from one point to the next
+     * as given by the order in the array.
+     * Substitute for Graphics.drawPolygon method
+     * @param xPoints are the x-coordinates of points for the polygon
+     * @param yPoints are the y-coordinates of points for the polygon
+     * @param nPoints is the number of points, the length of the arrays
+     */
     @Override
     public void addPolygon(int[] xPoints, int[] yPoints, int nPoints) {
-
+        // make the Paint style stroke
+        paint.setStyle(Paint.Style.STROKE);
+        // will create a polygon by using Path object
+        Path polygonPath = new Path();
+        polygonPath.reset();
+        // using moveTo method for the first point
+        polygonPath.moveTo(xPoints[0], yPoints[0]);
+        // for the rest of the points, use lineTo method
+        for (int i = 1; i < nPoints; i++) {
+            polygonPath.lineTo(xPoints[i], yPoints[i]);
+        }
+        // then use lineTo for the first point
+        polygonPath.lineTo(xPoints[0], yPoints[0]);
+        // then draw polygon on canvas
+        canvas.drawPath(polygonPath, paint);
     }
 
+    /**
+     * Adds a line.
+     * A line is described by {@code (x,y)} coordinates for its
+     * starting point and its end point.
+     * Substitute for Graphics.drawLine method
+     * @param startX is the x-coordinate of the starting point
+     * @param startY is the y-coordinate of the starting point
+     * @param endX is the x-coordinate of the end point
+     * @param endY is the y-coordinate of the end point
+     */
     @Override
     public void addLine(int startX, int startY, int endX, int endY) {
-
+        canvas.drawLine(startX, startY, endX, endY, paint);
     }
 
+    /**
+     * Adds a filled oval.
+     * The oval is specified with the {@code (x,y)} coordinates
+     * of the upper left corner and then its width for the
+     * x-axis and the height for the y-axis. An oval is
+     * described like a rectangle.
+     * Substitute for Graphics.fillOval method
+     * @param x is the x-coordinate of the top left corner
+     * @param y is the y-coordinate of the top left corner
+     * @param width is the width of the oval
+     * @param height is the height of the oval
+     */
     @Override
     public void addFilledOval(int x, int y, int width, int height) {
-
+        // set paint type to fill
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawOval((float)x, (float)y, (float)x + (float)width, (float)y + (float)height, paint);
+        //////////////////////////////////////////////////////////////////// *** AGAIN, SHOULD IT BE Y - HEIGHT???*******
     }
 
+    /**
+     * Adds the outline of a circular or elliptical arc covering the specified rectangle.
+     * The resulting arc begins at startAngle and extends for arcAngle degrees,
+     * using the current color. Angles are interpreted such that 0 degrees
+     * is at the 3 o'clock position. A positive value indicates a counter-clockwise
+     * rotation while a negative value indicates a clockwise rotation.
+     * The center of the arc is the center of the rectangle whose origin is
+     * (x, y) and whose size is specified by the width and height arguments.
+     * The resulting arc covers an area width + 1 pixels wide
+     * by height + 1 pixels tall.
+     * The angles are specified relative to the non-square extents of
+     * the bounding rectangle such that 45 degrees always falls on the
+     * line from the center of the ellipse to the upper right corner of
+     * the bounding rectangle. As a result, if the bounding rectangle is
+     * noticeably longer in one axis than the other, the angles to the start
+     * and end of the arc segment will be skewed farther along the longer
+     * axis of the bounds.
+     * Substitute for Graphics.drawArc method
+     * @param x the x coordinate of the upper-left corner of the arc to be drawn.
+     * @param y the y coordinate of the upper-left corner of the arc to be drawn.
+     * @param width the width of the arc to be drawn.
+     * @param height the height of the arc to be drawn.
+     * @param startAngle the beginning angle.
+     * @param arcAngle the angular extent of the arc, relative to the start angle.
+     */
     @Override
     public void addArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-
+        // i THINK i set the paint to fill for this
+        paint.setStyle(Paint.Style.FILL);
+        // I THINK I USE FALSE BOOLEAN FOR USECENTER
+        canvas.drawArc((float)x, (float)y, (float)x + (float)width, (float)y + (float)height, startAngle, arcAngle, false, paint);
     }
 
+    /**
+     * Adds a string at the given position.
+     * Substitute for CompassRose.drawMarker method
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param str the string
+     */
     @Override
     public void addMarker(float x, float y, String str) {
-
+        // set paint type to stroke
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawText(str, x, y, paint);
     }
 
+    /**
+     * Sets the value of a single preference for the rendering algorithms.
+     * It internally maps given parameter values into corresponding java.awt.RenderingHints
+     * and assigns that to the internal graphics object.
+     * Hint categories include controls for rendering quality
+     * and overall time/quality trade-off in the rendering process.
+     *
+     * Refer to the awt RenderingHints class for definitions of some common keys and values.
+     *
+     * Note for Android: start with an empty default implementation.
+     * Postpone any implementation efforts till the Android default rendering
+     * results in unsatisfactory image quality.
+     *
+     * @param hintKey the key of the hint to be set.
+     * @param hintValue the value indicating preferences for the specified hint category.
+     */
     @Override
     public void setRenderingHint(P7RenderingHints hintKey, P7RenderingHints hintValue) {
-
+        // ************* DON'T KNOW WHAT TO DO RIGHT NOW **************
     }
 }
 
